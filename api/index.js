8e8eb8180c7aa6098ec8837280eb39fc5e01d896
Hassan Ali <hassanali14563@gmail.com>
2023-11-08 11:40:59 +0500

    connecting to the db

--- api/index.js
+++ api/index.js
@@ -3,7 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const port = process.env.PORT || 8080;
+// connecting to the db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => console.log(`Server is listening on ${port}...`));
+  })
+  .catch((err) => console.log(err));
+// routes
 app.use("/", (req, res) => {
   return res.status(200).send("Everything is working just perfect");
 });
-app.listen(port, () => console.log("Server is listening"));
