02c22239f27f0acedaa10db45a4b56406cbb7594
hassanalee <hassanali14563@gmail.com>
2023-11-13 16:01:13 +0500

    adding router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home";
+import Authors from "./pages/Authors";
+import Profile from "./pages/Profile";
+import Blogs from "./pages/Blogs";
+import Header from "./components/Header";
 const App = () => {
-  return <div className="text-3xl bg-red-700">App</div>;
+  return (
+    <>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Header>
+    </>
+  )
 };
 
 export default App;
