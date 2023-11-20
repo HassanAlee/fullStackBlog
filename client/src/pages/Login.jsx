ff308f8666bd396ee242dfbea7347c9b8b5012e8
hassanalee <hassanali14563@gmail.com>
2023-11-20 16:18:58 +0500

    completed register and login pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
-const Login = () => {
+import { Link } from 'react-router-dom'
+const Register = () => {
     return (
-        <div>Login</div>
+        <section className='h-screen w-full  flex justify-between flex-col sm:flex-row'>
+            {/* form side */}
+            <article className='flex-1 h-full'>
+                <div className='h-full w-full flex items-center justify-center'>
+                    <form className='text-sm w-full sm:w-[60%] px-4 sm:px-0'>
+                        <h1 className='capitalize font-medium text-3xl'>Welcome back!</h1>
+                        <h5 className='mb-8 mt-2'>Enter your credentials to access your account</h5>
+                        <div className='flex flex-col mb-4'>
+                            <label htmlFor="name" className='mb-1'>Email</label>
+                            <input type="email" id='email' name='email' className='rounded-lg py-2 px-3 outline-none border' />
+                        </div>
+                        <div className='flex flex-col mb-4'>
+                            <label htmlFor="password" className='mb-1'>Password</label>
+                            <input type="text" id='password' name='password' className='rounded-lg py-2 px-3 outline-none border' />
+                        </div>
+                        <button className='w-full rounded-2xl mt-3 bg-[#4b6bfb] capitalize text-white py-2 font-medium text-lg hover:opacity-70'>signup</button>
+                        <p className='mt-3 text-sm text-center'>Don't have an account? <Link className='text-[#4b6bfb]' to={"/register"}>Sign up</Link></p>
+                    </form>
+                </div>
+            </article >
+            {/* image side */}
+            <article className='flex-1 h-full hidden sm:block'>
+                <img src="images/form.png" alt="flowers_img" className='h-full w-full' />
+            </article>
+        </section>
     )
 }
 
-export default Login
+export default Register
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,24 +6,25 @@ const Register = () => {
             {/* form side */}
             <article className='flex-1 h-full'>
                 <div className='h-full w-full flex items-center justify-center'>
-                    <form className='text-sm w-full sm:w-auto px-4 sm:px-0'>
+                    <form className='text-sm w-full sm:w-[60%] px-4 sm:px-0'>
+                        <h1 className='capitalize font-medium text-3xl mb-8'>get started now</h1>
                         <div className='flex flex-col mb-4'>
                             <label htmlFor="name" className='mb-1'>Name</label>
-                            <input type="text" id='name' name='name' className='rounded-lg py-1 px-3 outline-none border' />
+                            <input type="text" id='name' name='name' className='rounded-lg py-2 px-3 outline-none border' />
                         </div>
                         <div className='flex flex-col mb-4'>
                             <label htmlFor="name" className='mb-1'>Email</label>
-                            <input type="email" id='email' name='email' className='rounded-lg py-1 px-3 outline-none border' />
+                            <input type="email" id='email' name='email' className='rounded-lg py-2 px-3 outline-none border' />
                         </div>
                         <div className='flex flex-col mb-4'>
                             <label htmlFor="password" className='mb-1'>Password</label>
-                            <input type="text" id='password' name='password' className='rounded-lg py-1 px-3 outline-none border' />
+                            <input type="text" id='password' name='password' className='rounded-lg py-2 px-3 outline-none border' />
                         </div>
                         <div className='flex flex-col mb-4'>
-                            <label htmlFor="password" className='mb-1'>Country</label>
-                            <input type="text" id='country' name='country' className='rounded-lg py-1 px-3 outline-none border' />
+                            <label htmlFor="country" className='mb-1'>Country</label>
+                            <input type="text" id='country' name='country' className='rounded-lg py-2 px-3 outline-none border' />
                         </div>
-                        <button className='w-full rounded-xl mt-3 bg-[#4b6bfb] capitalize'>signup</button>
+                        <button className='w-full rounded-2xl mt-3 bg-[#4b6bfb] capitalize text-white py-2 font-medium text-lg hover:opacity-70'>signup</button>
                         <p className='mt-3 text-sm text-center'>Have an account? <Link className='text-[#4b6bfb]' to={"/login"}>Sign in</Link></p>
                     </form>
                 </div>
