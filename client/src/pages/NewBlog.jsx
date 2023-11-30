ca7bf5e53447680ed58b5bc928d94712fbf9aea2
hassanalee <hassanali14563@gmail.com>
2023-11-30 15:36:59 +0500

    added categories

--- client/src/pages/NewBlog.jsx
+++ client/src/pages/NewBlog.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react'
 import Button from '../components/Button'
 import { FaImage } from "react-icons/fa6";
+import { useSelector } from 'react-redux'
+const catList = ["Technology", "Motivation", "Entertainment", "Sports", "Traveling"]
 const NewBlog = () => {
+    const { currentUser } = useSelector((state) => state.userSlice)
+    const [blogData, setBlogData] = useState(() => {
+        return {
+            title: "",
+            description: "",
+            authorRef: "",
+            image: "",
+            category: ""
+        }
+    })
+    // change handler to update the state
+    const handleChange = (e) => {
+        setBlogData({ ...blogData, [e.target.name]: e.target.value, authorRef: currentUser._id })
+    }
+    // submit handler to create the blog
+    const handleSubmit = () => {
+        console.log(blogData);
+    }
     return (
         <section className=' flex justify-between flex-col sm:flex-row p-4 sm:p-0'>
             <article className=' w-full sm:w-3/5'>
-                <input type="text" placeholder='Add Title' name='title' id='title' className='bg-[#f6f6f7] w-full px-5 py-3 text-2xl outline-0 mb-4 rounded-md' /> <br />
-                <textarea name="desc" id="desc" placeholder='Start writing your blog here'
+                <input type="text" placeholder='Add Title' name='title' id='title' className='bg-[#f6f6f7] w-full px-5 py-3 text-2xl outline-0 mb-4 rounded-md' value={blogData.title} onChange={handleChange} /> <br />
+                <textarea name="description" id="description" placeholder='Start writing your blog here'
                     className='bg-[#f6f6f7] w-full px-5 py-3 text-base outline-0 mb-4 rounded-md resize-none' rows={15}
+                    value={blogData.description}
+                    onChange={handleChange}
                 >
                 </textarea>
             </article>
@@ -19,8 +41,13 @@ const NewBlog = () => {
                     </label>
                     <input type="file" id='image' name='image' accept='image/*' className='invisible' />
                 </div>
+                <select name="category" id="category" defaultValue={"technology"} className='mb-10 w-full outline-0 p-2'>
+                    {
+                        catList.map((item, i) => <option value={item.toLocaleUpperCase()} key={i}>{item}</option>)
+                    }
+                </select>
                 <div className='flex gap-x-4 justify-center'>
-                    <Button text={"publish"} />
+                    <Button text={"publish"} click={handleSubmit} />
                     <Button text={"cancel"} />
                 </div>
             </article>
