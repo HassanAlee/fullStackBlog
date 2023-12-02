9d411bc0838380d788bbe5a23370b411d992b995
hassanalee <hassanali14563@gmail.com>
2023-12-02 11:23:35 +0500

    uploading image successfully

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBddjDrRoiDpq8Xmke1YPq9v--haZGsxOs",
+  authDomain: "fullstackblog-f877f.firebaseapp.com",
+  projectId: "fullstackblog-f877f",
+  storageBucket: "fullstackblog-f877f.appspot.com",
+  messagingSenderId: "303731645452",
+  appId: "1:303731645452:web:f1d222651787ae5acd2229",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/NewBlog.jsx
+++ client/src/pages/NewBlog.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '../components/Button'
 import { FaImage } from "react-icons/fa6";
 import { useSelector } from 'react-redux'
+import { app } from '../firebase';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from 'firebase/storage';
 const catList = ["Technology", "Motivation", "Entertainment", "Sports", "Traveling"]
 const NewBlog = () => {
     const { currentUser } = useSelector((state) => state.userSlice)
@@ -14,14 +21,50 @@ const NewBlog = () => {
             category: ""
         }
     })
+    // firebase related
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    // image upload function
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setBlogData({ ...blogData, image: downloadURL })
+                    setFile('')
+                }
+                );
+            }
+        );
+    };
+    // useEffect to upload image
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file)
+        }
+    }, [file])
     // change handler to update the state
     const handleChange = (e) => {
         setBlogData({ ...blogData, [e.target.name]: e.target.value, authorRef: currentUser._id })
     }
     // submit handler to create the blog
     const handleSubmit = () => {
-        console.log(blogData);
-    }
+        console.log({ ...blogData, authorRef: currentUser._id });
+    };
     return (
         <section className=' flex justify-between flex-col sm:flex-row p-4 sm:p-0'>
             <article className=' w-full sm:w-3/5'>
@@ -37,13 +80,17 @@ const NewBlog = () => {
                 <p className='text-center text-[#8395a7] text-sm'>Click the icon below to upload an image</p>
                 <div className='text-center'>
                     <label htmlFor="image" className=' text-center'>
-                        <FaImage className='w-3/4 text-[#8395a7] h-1/4 mx-auto cursor-pointer' />
+                        {
+                            !blogData.image ? !file ? <FaImage className='w-3/4 text-[#8395a7] h-1/4 mx-auto cursor-pointer' /> : <p className='mt-4 text-sm text-green-500'>Please wait till image is uploaded</p> : !fileUploadError ? <img src={blogData.image} alt='image' className='mt-4' /> : <p className='mt-4 text-sm text-red-500'>Something went wrong while uploading</p>
+                        }
                     </label>
-                    <input type="file" id='image' name='image' accept='image/*' className='invisible' />
+                    <input type="file" id='image' name='image' accept='image/*' className='invisible' onChange={(e) => {
+                        setFile(e.target.files[0])
+                    }} />
                 </div>
-                <select name="category" id="category" defaultValue={"technology"} className='mb-10 w-full outline-0 p-2'>
+                <select name="category" id="category" defaultValue={"technology"} className='mb-10 w-full outline-0 p-2' onChange={(e) => setBlogData({ ...blogData, category: e.target.value })}>
                     {
-                        catList.map((item, i) => <option value={item.toLocaleUpperCase()} key={i}>{item}</option>)
+                        catList.map((item, i) => <option value={item} key={i}>{item}</option>)
                     }
                 </select>
                 <div className='flex gap-x-4 justify-center'>
