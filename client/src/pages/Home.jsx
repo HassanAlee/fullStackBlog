b9015ad49e357b3b4625c8d10c23b54b5f453f57
hassanalee <hassanali14563@gmail.com>
2023-12-06 12:14:17 +0500

    created ui for open blog page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,20 +14,24 @@ const Home = () => {
     }
   }, [])
   let random = Math.floor(Math.random() * blogs.length)
-  const formattedDate = getDateFormat(blogs[random].createdAt)
+  let formattedDate = getDateFormat(blogs[random]?.createdAt)
+  useEffect(() => {
+    Math.floor(Math.random() * blogs.length)
+    formattedDate = getDateFormat(blogs[random]?.createdAt)
+  }, [blogs])
   return (
     <>
       {/* top section */}
       <section className='h-[670px] w-full relative mb-28 sm:mb-0'>
-        <img src={blogs[random].image || "images/Image.png"} alt="main" className='h-[80vh] w-full object-cover rounded-2xl' />
+        <img src={blogs[random]?.image || "images/Image.png"} alt="main" className='h-[80vh] w-full object-cover rounded-2xl' />
         {/* blog info */}
         <div className='absolute sm:bottom-[15%] bottom-[-15%] left-[4%] rounded-md bg-white w-[96%] sm:w-[600px] flex flex-col gap-y-4 p-8 shadow-md'>
-          <Button text={blogs[random].category} />
-          <h1 className='text-4xl font-medium'>{blogs && blogs[random].title}</h1>
+          <Button text={blogs[random]?.category} />
+          <h1 className='text-4xl font-medium'>{blogs && blogs[random]?.title}</h1>
           <div className='flex items-center justify-between w-full sm:w-1/2'>
-            <img src={blogs[random].authorImage || "images/user.png"} alt="user1" className='h-[50px] w-[50px] rounded-full' />
-            <p className='capitalize text-[#97989F] font-medium'>{blogs && blogs[random].authorName}</p>
-            <p className='text-sm text-[#97989F] font-medium'>{formattedDate.toLocaleString('en-US', { month: 'long' })}{" "}{formattedDate.getDate()},{" "}{formattedDate.getFullYear()}</p>
+            <img src={blogs[random]?.authorImage || "images/user.png"} alt="user1" className='h-[50px] w-[50px] rounded-full' />
+            <p className='capitalize text-[#97989F] font-medium'>{blogs && blogs[random]?.authorName}</p>
+            <p className='text-sm text-[#97989F] font-medium'>{formattedDate?.toLocaleString('en-US', { month: 'long' })}{" "}{formattedDate.getDate()},{" "}{formattedDate?.getFullYear()}</p>
           </div>
         </div>
       </section>
--- client/src/pages/OpenBlog.jsx
+++ client/src/pages/OpenBlog.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAllBlogs } from '../redux-toolkit/features/blogsSlice'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { getDateFormat } from '../utils/getDateFormat'
 const OpenBlog = () => {
     const { id } = useParams()
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const { blogs } = useSelector((state) => state.blogsSlice)
     useEffect(() => {
@@ -11,12 +14,26 @@ const OpenBlog = () => {
             dispatch(getAllBlogs())
         }
     }, [])
-    const thisBlog = blogs.find((item => item._id == id))
+    let thisBlog = blogs.find((item => item._id == id))
+    let formattedDate = getDateFormat(thisBlog.createdAt);
+    useEffect(() => {
+        thisBlog = blogs.find((item => item._id == id))
+        formattedDate = getDateFormat(thisBlog.createdAt)
+    }, [blogs])
     return (
-        <div>
-            <h1>{thisBlog.title}</h1>
-            <h2>{thisBlog.authorName}</h2>
-        </div>
+        <section className='h-full px-4 '>
+            <h4 className='bg-[#4b6bfb] inline-block px-2 py-1 rounded-md text-white text-xs'>{thisBlog.category}</h4>
+            <h2 className='font-semibold text-4xl my-5'>{thisBlog.title}</h2>
+            <div className='flex items-center gap-x-5 w-full'>
+                <img src={thisBlog.authorImage || "images/user.png"} alt="user1" className='h-[50px] w-[50px] rounded-full object-cover' />
+                <p className='capitalize text-[#97989F] font-medium hover:cursor-pointer hover:text-[#4b6bfb] hover:underline' onClick={() => navigate(`/authors/${thisBlog.authorRef}`)}>{blogs && thisBlog.authorName}</p>
+                <p className='text-sm text-[#97989F] font-medium'>{formattedDate.toLocaleString('en-US', { month: 'long' })}{" "}{formattedDate.getDate()},{" "}{formattedDate.getFullYear()}</p>
+            </div>
+            <div className='w-full h-50vh sm:h-[70vh] my-10'>
+                <img src={thisBlog.image} alt="title_image" className='h-full w-full rounded-xl' />
+            </div>
+            <h1 className='text-justify text-xl mb-10'>{thisBlog.description}</h1>
+        </section>
     )
 }
 
