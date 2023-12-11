85d41c1e5afaf3f0bcefdc29c503cb26342fdc3e
hassanalee <hassanali14563@gmail.com>
2023-12-12 04:54:17 +0500

    fixed date formats

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -22,13 +22,13 @@ const Blogs = () => {
       {/* top head section */}
       <div className='h-[60vh] relative'>
         <img src={blogs[random].image} alt="image" className='h-full w-full object-cover bg-center rounded-xl' />
-        <div className='absolute top-1/2 sm:top-2/3 left-2 sm:left-8 h-full w-full'>
+        <div className='absolute top-1/2 sm:top-[60%] left-2 sm:left-8 h-full w-full'>
           <h1 className='bg-[#4b6bfb] text-white p-1 rounded-md inline-block capitalize '>{blogs[random].category}</h1>
           <h1 className='my-1 text-white first-letter:capitalize text-2xl font-bold'>{blogs[random]?.title}</h1>
           <div className='flex items-center gap-x-2 text-white text-sm'>
             <img src={blogs[random]?.authorImage || "/images/user.png"} alt={blogs[random]?.authorName} className='h-[60px] w-[60px] rounded-full' />
             <p>{blogs[random]?.authorName}</p>
-            <p>august 22,2022</p>
+            <p className='text-sm text-white font-medium'>{formattedDate?.toLocaleString('en-US', { month: 'long' })}{" "}{formattedDate.getDate()},{" "}{formattedDate?.getFullYear()}</p>
           </div>
         </div>
       </div>
