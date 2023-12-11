de8b67089ef2c34df4f95b9d2a3466eda6b88311
hassanalee <hassanali14563@gmail.com>
2023-12-12 03:43:54 +0500

    created backend api for updating the blogs

--- api/controllers/blog.controller.js
+++ api/controllers/blog.controller.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/blog.model.js");
+const { errorHandler } = require("../utils/error.js");
 // add a new blog
 const addBlog = async (req, res, next) => {
   try {
@@ -24,4 +25,24 @@ const getAllBlogs = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { addBlog, getAllBlogs };
+// update a blog
+const updateBlog = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return next(errorHandler(404, "Blog not found"));
+    }
+    if (blog.authorRef != req.user.id) {
+      return next(errorHandler(401, "You can only update your own blog"));
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runVAlidators: true,
+    });
+    return res.status(200).json(updatedBlog);
+  } catch (error) {
+    next(error.message);
+  }
+};
+module.exports = { addBlog, getAllBlogs, updateBlog };
--- api/routes/blog.routes.js
+++ api/routes/blog.routes.js
@@ -1,6 +1,12 @@
-const { addBlog, getAllBlogs } = require("../controllers/blog.controller.js");
+const {
+  addBlog,
+  getAllBlogs,
+  updateBlogs,
+  updateBlog,
+} = require("../controllers/blog.controller.js");
 const { verifyToken } = require("../utils/verifyToken.js");
 const router = require("express").Router();
 router.post("/add-blog", verifyToken, addBlog);
 router.get("/get-blogs", getAllBlogs);
+router.patch("/update-blog/:id", verifyToken, updateBlog);
 module.exports = router;
